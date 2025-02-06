e4707a243dfc8fe5fb0e97535e3dbc38f27b4cfd
gab0419 <gabriel.lawrence04@outlook.com>
2025-02-06 15:08:30 +0800

    Update(Jammil)
    - Add About Button
    - Add the Controller and Keybind Selection

--- Slime Tales/Scripts/main_menu.gd
+++ Slime Tales/Scripts/main_menu.gd
@@ -8,7 +8,10 @@ func _on_start_button_pressed() -> void:
 #Option
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
-	
+
+func _on_about_button_pressed() -> void:
+	pass # Replace with function body.
+		
 #Exit
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
